326cdcd8c8b55e6047dac71fbef948519ae42f52
vissu255 <viswanathbuchi@gmail.com>
2024-07-03 18:00:44 +0530

    modification/creation

--- src/pages/Cart.java
+++ src/pages/Cart.java
@@ -2,4 +2,7 @@ package pages;
 
 public class Cart {
 
+	public static void main(String[] args) {
+		System.out.println("Cart Page");
+	}
 }
---
+++ src/tests/CartTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class CartTest {
+
+	@Test
+	public void Carttest1(){
+		System.out.println("cart test 1");
+	}
+}
